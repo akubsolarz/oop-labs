eafcab2a24010e7930b255de4919c5c63c8ee429
akubsolarz <234617@student.uek.krakow.pl>
2025-10-22 21:00:17 +0200

    Stwory

---
+++ Simulator/Creature.cs
@@ -0,0 +1,33 @@
+﻿namespace Simulator
+{
+    public class Creature
+    {
+        public string Name { get; }
+        private int _level;
+
+        public int Level
+        {
+            get => _level;
+            set { _level = value > 0 ? value : 1; }
+        }
+
+        public Creature()
+        {
+            Name = "Unknown";
+            Level = 0;
+        }
+        public Creature(string name, int level)
+        {
+            Name = name;
+            Level = level;
+        }
+
+        public void SayHi() => Console.WriteLine($"Hi, I'm {Name}, my level is {Level}.");
+
+        public static void Slogan() => Console.WriteLine("creatures are great !!!");
+
+        public string Info => $"{Name} [{Level}]";
+
+
+    }
+}
---
+++ Simulator/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Simulator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting Simulator!\n");
+
+            Creature c = new Creature("shrek", 7);
+
+            c.SayHi();
+            
+            Console.WriteLine(c.Info);
+            Creature.Slogan();
+
+            Animals a = new() { Size = 3, Description = "Dogs" };
+            Console.WriteLine($"{a.Description}, {a.Size}");  // Rats, 3
+
+            Console.WriteLine(a.Info);
+
+            Console.WriteLine("Koniec");
+
+        }
+    }
+}
